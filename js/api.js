0e122c37f8a27a80fc97e317811a665f7806c1b1
Gyunnam <gyuni9604@naver.com>
2021-07-28 19:00:43 +0900

    레이아웃 변경, 데이터 추출

--- js/api.js
+++ js/api.js
@@ -8,7 +8,7 @@ function popularMovie(){//영화
         .then((res)=>{
             var popularMovie = res.data.results;
             var output = ''
-            for(var i = 0 ; i < 10 ; i++){
+            for(var i = 0 ; i < popularMovie.length ; i++){
                 output += '<li>'
                 output += '<a href ="#" id="'+ popularMovie[i].id+'">'
                 output += '<img src = "' + img + popularMovie[i].poster_path + '" alt ="poster" class="movie_poster">'
@@ -33,21 +33,24 @@ function popularTv(){//티비
             var popularTv = res.data.results
             var output = ''
 
-            for(var i = 0 ; i < 10 ; i++){
-                output += '<li>'
-                output += '<a href="#" id="'+ popularTv[i].id+'">'
-                output += '<img src = "' + img + popularTv[i].poster_path + '" alt ="poster" class="tv_poster">'
-                output += '<div class="tv_score">'
-                output += '<p>'+ popularTv[i].vote_average +'</p>'
-                output += '</div>'
-                output += '<div class="tv_detail">'
-                output += '<p>' + popularTv[i].name +'</p>'
-                output += '<p>' + popularTv[i].first_air_date + '</p>' +'</div>'
-                output += '</div>'
-                output += '</a>'
-                output += '</li>'
+            for(var i = 0 ; i < popularTv.length ; i++){
+                if(popularTv[i].poster_path != null){
+                    output += '<li>'
+                    output += '<a href="#" id="'+ popularTv[i].id+'">'
+                    output += '<img src = "' + img + popularTv[i].poster_path + '" alt ="poster" class="tv_poster">'
+                    output += '<div class="tv_score">'
+                    output += '<p>'+ popularTv[i].vote_average +'</p>'
+                    output += '</div>'
+                    output += '<div class="tv_detail">'
+                    output += '<p>' + popularTv[i].name +'</p>'
+                    output += '<p>' + popularTv[i].first_air_date + '</p>' +'</div>'
+                    output += '</div>'
+                    output += '</a>'
+                    output += '</li>'
 
-                $('.slide.second').html(output)
+                    $('.slide.second').html(output)
+                }
+                
             }
         })
 }
@@ -182,28 +185,120 @@ function tvNow(){
             }
         })
 }
-function tvUpcoming(){
-    axios.get(api_basic + 'tv/latest' + api_key)
+
+
+//search
+
+$(document).ready(()=>{
+    $('.search_form').submit((e)=>{
+        var searchTxt = $('.search_bar').val()
+        e.preventDefault()
+        searchGetMovie(searchTxt)
+        searchGetTv(searchTxt)
+    })
+})
+
+function searchGetMovie(searchTxt){
+    var frame =`
+            <div id="movie" class="content">
+            <span>Movie Result</span>
+            <div>
+            </div>
+            </div>
+            <div id="tv" class="content">
+            <span>TV Result</span>
+            <div>
+            </div>
+            </div>
+            `
+    $('#result').html(frame)
+    axios(api_basic + 'search/movie' + api_key + '&query=' + searchTxt + '&page=1')
         .then((res)=>{
             var data = res.data.results
-            var output = ''
+            console.log(data)
+            var output =''
             
-            for(var i = 0 ; i<20; i++){
-                if(data[i].poster_path != null){
-                    output += '<a href="#" id="'+ data[i].id + '">'
+            if(data.length == 0){
+                $('#result').html(frame)
+                var noresult = '<div class="no_result">검색 결과가 없습니다.</div>'
+                $('#movie > div').html(noresult)
+                console.log('없당!')
+            }else{
+                
+
+                for(var i = 0 ; i < data.length ; i++){
+                    
+                    output += '<a href="#" id="'+ data[i].id +'">'
                     output += '<div>'
                     output += '<div class="imgbox">'
                     output += '<div class="img" style="background-image: url('+ img+data[i].poster_path +')"></div>'
+                    output += '<div class="blank"></div>'
                     output += '<span>'
                     output += '<span role="img" aria-label="rating">★</span>'
-                    output += data[i].vote_average +'/10'
+                    output += data[i].vote_average + '/10'
                     output += '</span>'
-                    output += '<div class="blank"></div>'
                     output += '</div>'
+                    output += '<span>'+ data[i].title +'</span>'
+                    output += '<span>'+ data[i].release_date +'</span>'
                     output += '</div>'
                     output += '</a>'
-                    $('.upcoming_group').html(output)
+
+                    // output =
+                    // `
+                    //     <a href="#" id="${data[i].id}">
+                    //         <div>
+                    //             <div class="imgbox">
+                    //                 <div class="img" style="background-image: url(${img + data[i].poster_path})"></div>
+                    //                 <span>
+                    //                     <span role="img" aria-label="rating">★</span>
+                    //                     ${data[i].vote_average}/10
+                    //                 </span>
+                    //             </div>
+                    //             <span>${data[i].title}</span>
+                    //             <span>${data[i].release_date}</span>
+                    //         </div>
+                    //     </a>
+                    // `
+                    $('#movie > div').html(output)
                 }
+                
+            }
+        })
+    axios(api_basic + 'search/tv' + api_key + '&query=' + searchTxt + '&page=1')
+        .then((res)=>{
+            var data = res.data.results
+            console.log(data)
+            var output = ''
+            if(data.length == 0){
+                var noresult = '<div class="no_result">검색 결과가 없습니다.</div>'
+                $('#tv > div').html(noresult)
+                console.log('없당!')
+            }else{
+                for(var i = 0 ; i < data.length ; i++){
+                    
+                    output += '<a href="#" id="'+ data[i].id +'">'
+                    output += '<div>'
+                    output += '<div class="imgbox">'
+                    output += '<div class="img" style="background-image: url('+ img+data[i].poster_path +')"></div>'
+                    output += '<div class="blank"></div>'
+                    output += '<span>'
+                    output += '<span role="img" aria-label="rating">★</span>'
+                    output += data[i].vote_average + '/10'
+                    output += '</span>'
+                    
+                    output += '</div>'
+                    output += '<span>'+ data[i].name +'</span>'
+                    output += '<span>'+ data[i].first_air_date +'</span>'
+                    output += '</div>'
+                    output += '</a>'
+
+                    $('#tv > div').html(output)
+                }
+                
             }
         })
 }
+function searchGetTv(searchTxt){
+    
+}
+
